6b6325f127d0260457c7f20ce54d56a78e764433
Bashar.mohammad <bashar.mohammad@jobdiva.com>
2026-02-06 17:07:06 +0200

    Utility Types

---
+++ lesson09/build/js/copyright.js
@@ -0,0 +1,19 @@
+// Original code
+// const year = document.getElementById("year");
+// const thisYear = new Date().getFullYear();
+// year.setAttribute("datetime", thisYear);
+// year.textContent = thisYear;
+// Edited Code
+// let year: HTMLElement | null = document.getElementById("year");
+// let thisYear: string = new Date().getFullYear().toString();
+// if(year) {
+//     year.setAttribute("datetime", thisYear);
+//     year.textContent = thisYear;
+// }
+// 2nd Var
+const year = document.getElementById("year");
+let thisYear = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
+export {};
+//# sourceMappingURL=copyright.js.map
